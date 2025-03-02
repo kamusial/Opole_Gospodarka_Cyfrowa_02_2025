b46a78e01e6d49ae1a69b4040249451f9be30e0d
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-03-02 10:00:55 +0100

    '1'

---
+++ 0_tmp.py
@@ -0,0 +1,16 @@
+a = 5      # liczba całkowita
+b = 7.3    # liczba zmiennoprzecinkowa
+slowo = 'mama'  # string, napis
+
+print('a')
+print("a")
+print(a)
+
+print(a * b)
+print((3 + (a - 7)) / b)
+# print(a + slowo)
+print(a * slowo)
+
+moje_imie = input('Podaj imie')
+print('Cześć')
+print(moje_imie)
