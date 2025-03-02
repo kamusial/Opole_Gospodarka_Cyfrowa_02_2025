f3de240086b48ac84bda75465143b5e5d462dbc4
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-03-02 11:34:01 +0100

    '1'

---
+++ 2_otodom.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('otodom.csv')
+print(df.head().to_string())   # wyświetl 5 pierwszych wierszy
+
+print(df.describe().T.to_string())
+sns.heatmap(df.iloc[:, 1:].corr(), annot=True)
+plt.show()
